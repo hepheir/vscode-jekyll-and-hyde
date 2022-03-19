cd09ec40bf364d7e3dd1b27b1cdf35b25e730ea1
Hepheir <hepheir@gmail.com>
2022-03-19 15:22:54 +0900

    refactor(extension.ts): remove template code
    refactor(extension.ts): remove unused method `deactivate()`
    
    refactor(extension.ts): remove all operations in `activate()`

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,4 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-
-	let disposable = vscode.commands.registerCommand('jekyll-enthusiasm.helloWorld', () => {
-		vscode.window.showInformationMessage('Hello World from jekyll-enthusiasm!');
-	});
-
-	context.subscriptions.push(disposable);
 }
-
-export function deactivate() {}
