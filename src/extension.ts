ff302fc0444b291c080258f3e87fb5d91b163fc0
Hepheir <hepheir@gmail.com>
2022-03-22 14:57:50 +0900

    feat(extension.ts): use `JekyllExplorer`

--- src/extension.ts
+++ src/extension.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import { JekyllExplorer } from './jekyllExplorer';
+
 
 export function activate(context: vscode.ExtensionContext) {
+	new JekyllExplorer(context);
 }
