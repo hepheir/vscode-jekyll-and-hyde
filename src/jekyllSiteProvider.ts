87a994530bc5bd38dbea8636155b3976a125e5a1
Hepheir <hepheir@gmail.com>
2022-03-22 16:26:04 +0900

    feat(`JekyllSiteProvider`): create `CategoryItem` extends `TreeItem`

--- src/jekyllSiteProvider.ts
+++ src/jekyllSiteProvider.ts
@@ -1,4 +1,5 @@
-import { TreeDataProvider, TreeItem } from 'vscode';
+import { TreeDataProvider, TreeItem, TreeItemCollapsibleState } from 'vscode';
+import { Site } from 'jekyll';
 
 
 export class JekyllSiteProvider implements TreeDataProvider<TreeItem> {
@@ -8,5 +9,28 @@ export class JekyllSiteProvider implements TreeDataProvider<TreeItem> {
 
     async getChildren(element?: TreeItem): Promise<TreeItem[]> {
         return [];
+    }
+}
+
+
+class CategoryItem extends TreeItem {
+    public site: Site;
+    public category: string;
+
+    constructor(site: Site, category: string) {
+        super(category);
+
+        this.site = site;
+        this.category = category;
+
+        this.label = category;
+        this.description = `${site.categories[category].length} posts`;
+        this.collapsibleState = TreeItemCollapsibleState.Collapsed;
+    }
+
+    static getAllCategoryItemsFromSite(site: Site): CategoryItem[] {
+        const categories = Object.keys(site.categories);
+        const categoryItems = categories.map(category => new CategoryItem(site, category));
+        return categoryItems.sort((a, b) => a.category.localeCompare(b.category));
     }
 }
