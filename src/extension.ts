f1b547581ee9d8691299f1175c8a04b793722f4b
Hepheir <hepheir@gmail.com>
2022-03-23 23:31:01 +0900

    feat(extension.ts, package.json): use newly added views: `CategoryView`, `DraftView`, `PostView`, `PageView`

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,28 @@
 import * as vscode from 'vscode';
+import { JekyllSite } from './jekyllSite';
+import { CachedNodes } from './views/nodes/cachedNodes';
+import { CategoryView } from './views/categoryView';
+import { DraftView } from './views/draftView';
+import { PageView } from './views/pageView';
+import { PostView } from './views/postView';
 
 
 export function activate(context: vscode.ExtensionContext) {
+	const source = vscode.workspace.workspaceFolders && vscode.workspace.workspaceFolders.length > 0
+		? vscode.workspace.workspaceFolders[0].uri.fsPath
+		: undefined;
+
+	if (!source) {
+		return;
+	}
+
+	CachedNodes.cache(context, new JekyllSite(source));
+
+	new CategoryView(context, 'categoryView');
+	new DraftView(context, 'draftView');
+	new PageView(context, 'pageView');
+	new PostView(context, 'postView');
+
 	vscode.commands.registerCommand('showTextDocument', showTextDocument);
 }
 
