854a99eaf68545cab287bde5f6453a222c4b3023
Hepheir <hepheir@gmail.com>
2022-03-23 23:30:08 +0900

    refactor(extension.ts, package.json): remove deleted feature `JekyllExplorer`

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,7 @@
 import * as vscode from 'vscode';
-import { JekyllExplorer } from './jekyllExplorer';
 
 
 export function activate(context: vscode.ExtensionContext) {
-	new JekyllExplorer(context);
-
 	vscode.commands.registerCommand('showTextDocument', showTextDocument);
 }
 
