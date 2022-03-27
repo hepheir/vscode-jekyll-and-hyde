4f8f68b2f8952a42aaca4535b339c710d316bc94
Hepheir <hepheir@gmail.com>
2022-03-27 15:51:42 +0900

    refactor(src): create commands.ts, views.ts

--- src/extension.ts
+++ src/extension.ts
@@ -1,29 +1,13 @@
 import * as vscode from 'vscode';
-import { ReloadSiteCommand } from './commands/reloadSite';
-import { ShowTextDocumentCommand } from './commands/showTextDocument';
-import { UpdateViewsCommand } from './commands/updateViews';
-import { registerCommands } from './commands/base';
-import { CategoryView } from './views/categoryView';
-import { PageView } from './views/pageView';
-import { PostView } from './views/postView';
-import { DraftView } from './views/draftView';
-import { createViews } from './views/base';
+import * as commands from "./commands";
+import * as views from "./views";
 
 
 export function activate(context: vscode.ExtensionContext) {
-	const uses = [
-		ReloadSiteCommand,
-		ShowTextDocumentCommand,
-		UpdateViewsCommand,
-		CategoryView,
-		DraftView,
-		PageView,
-		PostView,
-	];
-	const reload = () => vscode.commands.executeCommand('reloadSite');
+	commands.registerCommands(context);
+	views.createViews(context);
 
-	registerCommands(context);
-	createViews(context);
+	const reload = () => vscode.commands.executeCommand('reloadSite');
 
 	vscode.workspace.onDidSaveTextDocument(reload);
 	reload();
