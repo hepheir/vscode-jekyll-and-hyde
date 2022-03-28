0b062c100c05047fe6f13472df5f146815cd34f6
Hepheir <hepheir@gmail.com>
2022-03-29 01:10:50 +0900

    feat: use `CategorizedPosts`

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,7 @@
 import * as vscode from 'vscode';
+import { CategorizedPosts } from './views/categorizedPosts';
 
 
 export function activate(context: vscode.ExtensionContext) {
+	new CategorizedPosts(context);
 }
