1e0ff9b573d7ab7f3e75480d8d018014ed2f88ff
Hepheir <hepheir@gmail.com>
2022-03-29 01:05:59 +0900

    refactor(extension.ts): reset `activate()`

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,5 @@
 import * as vscode from 'vscode';
-import * as commands from "./commands";
-import { Commands } from './constants';
-import * as views from "./views";
 
 
 export function activate(context: vscode.ExtensionContext) {
-	commands.registerCommands(context);
-	views.createViews(context);
-
-	const reload = () => vscode.commands.executeCommand(Commands.ReloadSite);
-
-	vscode.workspace.onDidSaveTextDocument(reload);
-	reload();
 }
