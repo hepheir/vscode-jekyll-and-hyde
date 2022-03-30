24c398ced16248ac594a6f21bf4d02ed9d62cb4f
Hepheir <hepheir@gmail.com>
2022-03-30 23:19:01 +0900

    refactor: show message if workspace folder not found

--- src/extension.ts
+++ src/extension.ts
@@ -3,5 +3,9 @@ import { CategorizedPosts } from './views/categorizedPosts';
 
 
 export function activate(context: vscode.ExtensionContext) {
+	if (!vscode.workspace.workspaceFolders) {
+		vscode.window.showInformationMessage("Could not find workspace folder.");
+		return;
+	}
 	new CategorizedPosts(context);
 }
