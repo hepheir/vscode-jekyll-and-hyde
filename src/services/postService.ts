5e4cd2f19d4e97f83512fca9e62c13b64fc10139
Hepheir <hepheir@gmail.com>
2022-12-20 21:26:08 +0900

    refactor: convert `PostService` into interface

--- src/services/postService.ts
+++ src/services/postService.ts
@@ -1,90 +1,19 @@
 import * as vscode from "vscode";
-import type { PostRepository } from "../models/post/postRepository";
-import { ArrayPostRepository } from "../models/post/arrayPostRepository";
-import { PostFileWriter } from "../models/post/postFileWriter";
-import { RepositoryError } from "../models/repositoryError";
-import { PostFileReader } from "../models/post/postFileReader";
+import type { PostDTO } from "../models/post/postDTO";
 
 /**
  * - *Note*: this service will affect neither `CategoryService` nor `CategoryRepository`.
  */
-export class PostService {
-    private readonly postRepository: PostRepository;
-
-    constructor() {
-        this.postRepository = new ArrayPostRepository();
-    }
-
-    add = (uri: vscode.Uri) => {
-        const newPost = new PostFileReader(uri).read();
-        this.postRepository.add(newPost);
-        return newPost;
-    }
-
-    list = () => {
-        return this.postRepository.list();
-    }
-
-    find = (uri: vscode.Uri) => {
-        return this.postRepository.findByUri(uri);
-    }
-
-    private ensureFind = (uri: vscode.Uri) => {
-        return this.postRepository.ensureFind(post => post.uri.fsPath === uri.fsPath);
-    }
-
-    getTitle = (
-        uri: vscode.Uri,
-    ) => {
-        return this.ensureFind(uri).title;
-    }
-
-    setTitle = (
-        uri: vscode.Uri,
-        title: string,
-    ) => {
-        const post = this.ensureFind(uri);
-        post.title = title;
-        new PostFileWriter(post, true).write();
-        this.postRepository.update(post);
-    }
-
-    getCategories = (
-        uri: vscode.Uri,
-    ) => {
-        return this.ensureFind(uri).categories;
-    }
-
-    addCategory = (
-        uri: vscode.Uri,
-        categoryName: string
-    ) => {
-        const post = this.ensureFind(uri);
-        post.categories.push(categoryName);
-        new PostFileWriter(post, true);
-        this.postRepository.update(post);
-    }
-
-    replaceCategory = (
-        uri: vscode.Uri,
-        oldCategoryName: string,
-        newCategoryName: string,
-    ) => {
-        const post = this.ensureFind(uri);
-        const categoryIndex = post.categories.findIndex(name => name === oldCategoryName);
-        if (categoryIndex === -1) {
-            throw new RepositoryError.ItemNotFound();
-        }
-        post.categories[categoryIndex] = newCategoryName;
-        this.postRepository.update(post);
-    }
-
-    removeCategory = (
-        uri: vscode.Uri,
-        categoryName: string,
-    ) => {
-        const post = this.ensureFind(uri);
-        post.categories = post.categories.filter(name => name !== categoryName);
-        this.postRepository.update(post);
-    }
+export interface PostService {
+    create: (uri: vscode.Uri, title: string, categories: readonly string[]) => PostDTO;
+    add: (uri: vscode.Uri) => void;
+    findAll: () => PostDTO[];
+    findByUri: (uri: vscode.Uri) => PostDTO | undefined;
+    findAllByCategories: (categories: readonly string[]) => PostDTO[];
+    setTitle: (uri: vscode.Uri, title: string) => void;
+    setCategories: (uri: vscode.Uri, categories: readonly string[]) => void;
+    addCategories: (uri: vscode.Uri, category: string) => void;
+    replaceCategories: (uri: vscode.Uri, oldCategory: string, newCategory: string) => void;
+    removeCategories: (uri: vscode.Uri, category: string) => void;
+    delete: (uri: vscode.Uri) => void;
 }
