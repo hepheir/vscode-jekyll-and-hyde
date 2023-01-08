9a57834cd4484e3a682c76e1f0562e4ba5fa7ef7
Hepheir <hepheir@gmail.com>
2023-01-08 16:06:51 +0900

    refactor: convert heap into abstract class

--- src/models/category/categoryDTOHeap.ts
+++ src/models/category/categoryDTOHeap.ts
@@ -2,17 +2,7 @@ import { Heap } from "../common/iterable/heap";
 import { CategoryDTO } from "./categoryDTO";
 
 export class CategoryDTOHeap extends Heap<CategoryDTO> {
-    override sort = (compareFn?: ((a: CategoryDTO, b: CategoryDTO) => number) | undefined) => {
-        if (compareFn === undefined) {
-            compareFn = (a, b) => {
-                return this.getCategoryId(a).localeCompare(this.getCategoryId(b));
-            };
-        }
-        super.sort(compareFn);
-        return this;
-    }
-
-    private getCategoryId = (entity: CategoryDTO) => {
-        return entity.names.join('/');
+    override getComparableKey = (category: CategoryDTO) => {
+        return category.names.join('/');
     }
 }
--- src/models/common/iterable/heap.ts
+++ src/models/common/iterable/heap.ts
@@ -1,7 +1,19 @@
-export class Heap<E> extends Array<E> {
+export abstract class Heap<E> extends Array<E> {
     override push(...items: E[]): number {
         super.push(...items);
         this.sort();
         return this.length;
     }
+
+    override sort = (compareFn?: ((a: E, b: E) => number) | undefined) => {
+        if (compareFn === undefined) {
+            compareFn = (a, b) => {
+                return this.getComparableKey(a).localeCompare(this.getComparableKey(b));
+            };
+        }
+        super.sort(compareFn);
+        return this;
+    }
+
+    abstract getComparableKey(elment: E): string;
 }
